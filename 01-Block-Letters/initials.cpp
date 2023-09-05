f10e4cbbee70ff93d9303382dcf018087d09e52d
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-09-05 18:05:13 +0100

    Add Block Letters Project

---
+++ 01-Block-Letters/initials.cpp
@@ -0,0 +1,11 @@
+// Insert Fun Fact Here.
+#include <iostream>
+
+int main(){
+  std::cout << "  JJJ  AAA   CCC   OOO  BBBB\n";
+  std::cout << "   J  A   A C   C O   O B   B\n";
+  std::cout << "   J  AAAAA C     O   O BBBB\n";
+  std::cout << "J  J  A   A C   C O   O B   B\n";
+  std::cout << " JJ   A   A  CCC   OOO  BBBB";
+  return 0;
+}
