5e38542b9a63a39e8c1decaa2f723f35b3041812
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-09-12 21:09:40 +0100

    Add drawBoard()

--- 12-Tic-Tac-Toe/utils.cpp
+++ 12-Tic-Tac-Toe/utils.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
 
+void drawBoard(std::vector<bool> board){
+  // Assume that true is X and false is O
+
+  std::string stringifiedBoard;
+  for(int i=0; i < 8; i++){
+    std::cout << i % 3 << '\n';
+  }
+}
+
+
+int main(){
+  std::vector<bool> board = {true,false,true,true,true,true,false,false,true};
+  drawBoard(board);
+}
