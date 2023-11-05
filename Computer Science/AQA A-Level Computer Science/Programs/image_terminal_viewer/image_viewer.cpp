aff1d0a624d3e92148243f0b5a3986b99e720b7b
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-05 21:32:52 +0000

    Start Adding Optimisation for Full Block and Transparency

--- Computer Science/AQA A-Level Computer Science/Programs/image_terminal_viewer/image_viewer.cpp
+++ Computer Science/AQA A-Level Computer Science/Programs/image_terminal_viewer/image_viewer.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include <sstream>
+#include <system_error>
 #include <vector>
 
 using std::string;
 using std::vector;
+
+struct Color{
+  string hex;
+  int r;
+  int g;
+  int b;
+  int alpha;
+
+  Color(std::string hex, int a){
+    hex = hex.substr(1);
+    r = std::stoi(hex.substr(0, 2), 0, 16);
+    g = std::stoi(hex.substr(2, 2), 0, 16);
+    b = std::stoi(hex.substr(4, 2), 0, 16);
+
+    try{
+      if(a != 0 || a != 1){
+        throw "Alpha must be between 0 - 2";
+      }
+      alpha = a;
+    } catch(const char* msg){
+      std::cerr << msg << std::endl;
+    }
+  }
+};
+
+string convertToPixel(Color top_pixel, Color bottom_pixel){
+  const wchar_t top_block = U'▀';
+  const wchar_t bottom_block = U'▄';
+  const wchar_t full_block = U'█';
+
+  std::stringstream ansii;
+  
+}
 
 string ANSIIString(vector<vector<string> > hex_bitmap){
 	const string pixel_character = "▀";
