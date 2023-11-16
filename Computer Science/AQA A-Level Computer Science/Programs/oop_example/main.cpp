69db8d462987842333a575411fc3bfea128df49a
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-16 19:59:16 +0000

    Add Vehicle

--- Computer Science/AQA A-Level Computer Science/Programs/oop_example/main.cpp
+++ Computer Science/AQA A-Level Computer Science/Programs/oop_example/main.cpp
@@ -7,33 +7,102 @@ using std::vector;
 class Key{
   public:
     Key(string uuid){
-    key = std::to_string(rand());  
-
-    for(char c : key){
-          keyhole += -c;
+      for(char c : uuid){
+          m_key += -c;
       }
     }
 
-    static string generateKeyUUID(int seed=time(NULL)){
+    Key() : m_key("") {}
+
+    static string generateKeyhole(int seed=time(NULL)){
       srand(seed);
       return std::to_string(rand());
     }
 
     string getKey(){
-      return key;
-    }
-
-    string getKeyhole(){
-      return keyhole;
+      return m_key;
     }
 
   private:
-    string key;
-    string keyhole;
+    string m_key;
+};
+
+class Vehicle{
+  public:
+    Vehicle(string registration_number, int max_gear, string keyhole=Key::generateKeyhole()) :
+      m_registration_number(registration_number),
+      m_max_gear(max_gear),
+      m_saved_key(Key()),
+      m_keyhole(keyhole)
+    {}
+
+    int getGear(){
+      return m_current_gear;
+    }
+
+    void gearUp(){
+      if(m_current_gear < m_max_gear){
+        m_current_gear++;
+      }
+    }
+
+    void gearDown(){
+      if(m_current_gear > -1){
+        m_current_gear--;
+      }
+    }
+
+    void goToGear(int gear){
+      if(gear <= m_max_gear || gear >= -1){
+        m_current_gear = gear;
+      }
+    }
+
+    string getRegistrationNumber(){
+      return m_registration_number;
+    }
+
+    void toggleAccelator(){
+      !m_accelerating;
+    }
+
+    bool getAccelerating(){
+      return m_accelerating;
+    }
+
+    void saveKey(Key key){
+      if(m_saved_key.getKey() != ""){
+        std::cerr << "Key already saved" << std::endl;
+      } else {
+        m_saved_key = key;
+      }
+    }
+
+    void toggleLock(Key key){
+      for(int i = 0; i < m_keyhole.size(); i++){
+        if(key.getKey()[i] != -m_keyhole[i]){
+          std::cerr << "Key does not match keyhole" << std::endl;
+          return;
+        }
+      }
+
+      !m_locked;
+    }
+
+    void toggleLoc(){
+      toggleLock(m_saved_key);
+    }
+
+  private:
+    string m_registration_number;
+    int m_current_gear = 0;
+    int m_max_gear;
+    Key m_saved_key;
+    bool m_accelerating = false;
+    string m_keyhole;
+    bool m_locked = true;
 };
 
 int main(){
-  Key key(Key::generateKeyUUID());
-  std::cout << key.getKey() << std::endl;
 }
 
