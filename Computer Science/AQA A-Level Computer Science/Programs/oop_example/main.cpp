10b93bdcba57a788872edf3dccee9d2a03193d53
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-16 19:14:10 +0000

    Add static method

--- Computer Science/AQA A-Level Computer Science/Programs/oop_example/main.cpp
+++ Computer Science/AQA A-Level Computer Science/Programs/oop_example/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 using std::string;
 using std::vector;
 
 class Key{
   public:
-    Key(string uuid) : key(uuid) {
-      for(char c : key){
+    Key(string uuid){
+    key = std::to_string(rand());  
+
+    for(char c : key){
           keyhole += -c;
       }
+    }
+
+    static string generateKeyUUID(int seed=time(NULL)){
+      srand(seed);
+      return std::to_string(rand());
     }
 
     string getKey(){
@@ -25,6 +33,7 @@ class Key{
 };
 
 int main(){
-  Key key;
+  Key key(Key::generateKeyUUID());
+  std::cout << key.getKey() << std::endl;
 }
 
