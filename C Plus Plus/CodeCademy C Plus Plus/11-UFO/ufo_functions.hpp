940bb7552fca0464ff59aaaa6c90cf5015081c65
Jacob O'Neill <jacoboneill2000@outlook.com>
2023-11-25 16:09:17 +0000

    Move C++ files to local files

--- C Plus Plus/CodeCademy C Plus Plus/11-UFO/ufo_functions.hpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-
-void display_misses(int misses);
-void greet();
-void end_game(std::string answer, std::string codeword);
-void display_status(std::vector<char> incorrect, std::string answer);
-void clear_screen();
-std::vector<int> codeword_check(std::string codeword, char letter);
--- C Plus Plus/CodeCademy C Plus Plus/12-Tic-Tac-Toe/utils.hpp
+++
@@ -1,7 +0,0 @@
-#include <vector>
-
-std::vector<char> draw(std::vector<char> board, bool is_player_1s_turn);
-bool togglePlayer(bool is_player_1s_turn);
-char isWin(std::vector<char> board);
-void drawWin(std::vector<char> board, char winner);
-void drawTie(std::vector<char> board);
